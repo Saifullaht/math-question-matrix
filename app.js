22774e44d57b27c4b8aaa4c92981487b856d1443
Saifullaht <mrsaifullah025@gmail.in>
2024-04-17 21:13:46 +0500

    math-question

---
+++ app.js
@@ -0,0 +1,24 @@
+let matrix1 = [
+    [2,4,6],
+    [8,1,8],
+    [2,2,5],
+];
+let matrix2 = [
+    [2,4,6],
+    [8,1,8],
+    [2,2,5],
+];
+let matrix3 = [
+    [5,9,6],
+    [2,5,5],
+    [4,5,8],
+];
+console.log(matrix1[0][0]+ matrix2[0][0] + matrix3[0][0]);
+console.log(matrix1[0][1]+ matrix2[0][1] + matrix3[0][1]);
+console.log(matrix1[0][2]+ matrix2[0][2] + matrix3[0][2]);
+console.log(matrix1[1][0]+ matrix2[1][0] + matrix3[1][0]);
+console.log(matrix1[1][1]+ matrix2[1][1] + matrix3[1][1]);
+console.log(matrix1[1][2]+ matrix2[1][2] + matrix3[1][2]);
+console.log(matrix1[2][0]+ matrix2[2][0] + matrix3[2][0]);
+console.log(matrix1[2][1]+ matrix2[2][1] + matrix3[2][1]);
+console.log(matrix1[2][2]+ matrix2[2][2] + matrix3[2][2]);
